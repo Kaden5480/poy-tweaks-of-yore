032c2b90511e41fef1078b59480ba9008f5a665a
Kaden5480 <kaden5480@protonmail.com>
2024-05-29 12:58:51 +0100

    Add pocket watch patch

---
+++ src/patches/Inventory.cs
@@ -0,0 +1,19 @@
+using HarmonyLib;
+
+namespace TweaksOfYore.Patches {
+    /**
+     * <summary>
+     * Disables using the pocket watch without using the keybind.
+     * </summary>
+     */
+    [HarmonyPatch(typeof(Inventory), "FetchPocketWatch")]
+    static class DisableInteractPocketWatch {
+        static bool Prefix(bool usedPocketWatchFromBelt) {
+            if (usedPocketWatchFromBelt == true) {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
